52ee0bba13145fc2757663d822e244fb5d025431
Ubuntu <ubuntu@ip-172-31-40-141.ap-south-1.compute.internal>
2019-08-10 07:25:55 +0000

    testnew

---
+++ src/main/java/com/javainuse/TestController.java
@@ -0,0 +1,18 @@
+package com.javainuse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public ModelAndView firstPage() {
+		return new ModelAndView("welcome"); 
+		//return "welcome";
+	}
+	
+
+}
